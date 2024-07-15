2a0d36b65c4058f60f9beefaa39a6cbf7c7669f4
Andy Roth <roth.andy@gmail.com>
2024-07-15 14:36:44 -0700

    wip

--- scripts/build_zarf_package.sh
+++ scripts/build_zarf_package.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 [-s|--skip-sbom]"
+    exit 1
+}
+
+# Initialize variable
+SKIP_SBOM=false
+
+# Parse input arguments
+while [[ "$1" != "" ]]; do
+    case $1 in
+        -s | --skip-sbom )
+            SKIP_SBOM=true
+            ;;
+        -h | --help )
+            usage
+            ;;
+        * )
+            usage
+            ;;
+    esac
+    shift
+done
+
 # Function to clean up Docker registry
 cleanup() {
   echo "Stopping and removing Docker registry..."
@@ -14,7 +39,7 @@ trap cleanup EXIT
 find_unused_port() {
   local port
   while : ; do
-    port=$(shuf -i 1024-65535 -n 1)
+    port=$(shuf -i 40000-65535 -n 1)
     (echo "" > /dev/tcp/127.0.0.1/$port) >/dev/null 2>&1
     if [ $? -ne 0 ]; then
       echo $port
@@ -50,8 +75,17 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+# Build the Zarf package creation command
+ZARF_COMMAND="uds zarf package create zarf -o zarf --registry-override=ghcr.io=localhost:${unused_port} --insecure --no-progress --confirm"
+
+if $SKIP_SBOM; then
+    ZARF_COMMAND="$ZARF_COMMAND --skip-sbom"
+fi
+
 # Creating the Zarf package
-uds zarf package create zarf -o zarf --registry-override=ghcr.io=localhost:${unused_port} --insecure --skip-sbom --no-progress --confirm
+echo "Executing: $ZARF_COMMAND"
+$ZARF_COMMAND
+
 if [ $? -ne 0 ]; then
   echo "Failed to create the Zarf package."
   exit 1
