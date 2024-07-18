bd13e2bf59f81ef5f4efe6f60619ff523ca7fcdc
Andy Roth <roth.andy@gmail.com>
2024-07-18 10:02:42 -0700

    ci: tag-push workflow, renovate updates, rename zarf package namespace (#33)

--- scripts/build_zarf_package.sh
+++ scripts/build_zarf_package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Due to relative paths this script is intended to be run from the root of the repository.
+
 # Function to display usage instructions
 usage() {
     echo "Usage: $0 [-s|--skip-sbom]"
@@ -36,11 +38,10 @@ cleanup() {
 trap cleanup EXIT
 
 # Function to find an unused port
-find_unused_port() {
+find_UNUSED_PORT() {
   local port=40000
   while [ $port -le 50000 ]; do
-    (echo "" > /dev/tcp/127.0.0.1/$port) >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
+    if ! (echo "" > /dev/tcp/127.0.0.1/$port) >/dev/null 2>&1; then
       echo $port
       return
     fi
@@ -51,47 +52,56 @@ find_unused_port() {
 }
 
 # Find an unused port
-unused_port=$(find_unused_port)
-echo "Using port $unused_port for Docker registry."
+UNUSED_PORT=$(find_UNUSED_PORT)
+echo "Using port $UNUSED_PORT for Docker registry."
+
+# set the registry, image name and tag
+REMOTE_DOCKER_REGISTRY="ghcr.io"
+LOCAL_DOCKER_REGISTRY="localhost:${UNUSED_PORT}"
+IMAGE_NAME="defenseunicorns/wfapi/containers/wfapi"
+IMAGE_TAG=$(yq e '.metadata.version' zarf/zarf.yaml)
+REMOTE_IMAGE_FULL_NAME="${REMOTE_DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
+LOCAL_IMAGE_FULL_NAME="${LOCAL_DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
+
+# Build the image
+if ! docker buildx build -f wfapi/Dockerfile -t "${REMOTE_DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}" .; then
+  echo "Failed to build the image."
+  exit 1
+fi
 
 # Run Docker registry
-docker run -d -p ${unused_port}:5000 --restart=always --name registry registry:2
-
-# Sleep for a couple of seconds to allow the registry to start
-sleep 3
-
-# Check if docker run succeeded
-if [ $? -ne 0 ]; then
+if ! docker run -d -p "${UNUSED_PORT}:5000" --restart=always --name registry registry:2; then
   echo "Failed to start Docker registry."
   exit 1
 fi
 
+# Sleep for a couple of seconds to allow the registry to start
+sleep 3
+
+
+
 # Tagging the image
-docker tag ghcr.io/defenseunicorns/wfapi/wfapi:$(yq e '.metadata.version' zarf/zarf.yaml) localhost:${unused_port}/defenseunicorns/wfapi/wfapi:$(yq e '.metadata.version' zarf/zarf.yaml)
-if [ $? -ne 0 ]; then
+if ! docker tag "${REMOTE_IMAGE_FULL_NAME}" "${LOCAL_IMAGE_FULL_NAME}"; then
   echo "Failed to tag the image."
   exit 1
 fi
 
 # Pushing the image
-docker push localhost:${unused_port}/defenseunicorns/wfapi/wfapi:$(yq e '.metadata.version' zarf/zarf.yaml)
-if [ $? -ne 0 ]; then
+if ! docker push "${LOCAL_IMAGE_FULL_NAME}"; then
   echo "Failed to push the image."
   exit 1
 fi
 
 # Build the Zarf package creation command
-ZARF_COMMAND="uds zarf package create zarf -o zarf --registry-override=ghcr.io=localhost:${unused_port} --insecure --no-progress --confirm"
+ZARF_COMMAND="uds zarf package create zarf -o zarf --registry-override=${REMOTE_DOCKER_REGISTRY}=${LOCAL_DOCKER_REGISTRY} --insecure --no-progress --confirm"
 
 if $SKIP_SBOM; then
-    ZARF_COMMAND="$ZARF_COMMAND --skip-sbom"
+  ZARF_COMMAND="$ZARF_COMMAND --skip-sbom"
 fi
 
 # Creating the Zarf package
 echo "Executing: $ZARF_COMMAND"
-$ZARF_COMMAND
-
-if [ $? -ne 0 ]; then
+if ! $ZARF_COMMAND; then
   echo "Failed to create the Zarf package."
   exit 1
 fi
