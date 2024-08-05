5bb9375e29321a2a716c4d579d1af5c5bfeb3847
Andy Roth <roth.andy@gmail.com>
2024-08-05 14:48:53 -0700

    wip

--- wfapi.E2ETests/V1/Controllers/WorkflowsControllerTests.cs
+++ wfapi.E2ETests/V1/Controllers/WorkflowsControllerTests.cs
@@ -82,7 +82,7 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
     }
 
     [Fact]
-    public async Task GetWorkflowLogstream_WhenCalledOnRunningPod_ReturnsOkImmediately()
+    public void GetWorkflowLogstream_WhenCalledOnRunningPod_ReturnsOkImmediately()
     {
         // Submit the workflow
         var workflow = (WorkflowInfo)Given()
@@ -107,7 +107,6 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
         while (workflow.Status != WorkflowStatus.Running && sw.ElapsedMilliseconds < 3000)
         {
             workflow = (WorkflowInfo)Given()
-                .Log(RequestLogLevel.All)
                 .Accept(MediaTypeNames.Application.Json)
                 .ContentType(MediaTypeNames.Application.Json)
                 .When()
@@ -122,12 +121,13 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
 
         // Get the logstream. Make sure it isn't waiting for the workflow to finish
         // We have to do our own request here because rest-assured-net doesn't support SSE
-        sw = Stopwatch.StartNew();
         var httpClientHandler = new HttpClientHandler();
         using var httpClient = new HttpClient(httpClientHandler, true);
         var request = new HttpRequestMessage(HttpMethod.Get, $"{RootUrl}/api/v1/workflows/{workflow.Name}/logstream");
         request.Headers.Add("Accept", "application/x-ndjson");
-        var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+        sw = Stopwatch.StartNew();
+        var response = httpClient.Send(request, HttpCompletionOption.ResponseHeadersRead);
+        sw.Stop();
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.NotNull(response.Content.Headers.ContentType?.MediaType);
         Assert.Equal("application/x-ndjson", response.Content.Headers.ContentType.MediaType);
@@ -136,7 +136,6 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
         // Istio strips this header. Not sure why yet. See https://defense-unicorns.slack.com/archives/C06QJAUHWFN/p1722893232750909
         // Assert.NotNull(response.Headers.Connection);
         // Assert.Equal("keep-alive", response.Headers.Connection.ToString());
-        sw.Stop();
         Assert.True(sw.ElapsedMilliseconds < 7000);
     }
 }
