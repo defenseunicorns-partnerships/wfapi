723caa1aed2c9711bb39840f992810df77edf3ff
Andy Roth <roth.andy@gmail.com>
2024-08-14 14:29:47 -0700

    wip

--- wfapi.E2ETests/V1/Controllers/WorkflowsControllerTests.cs
+++ wfapi.E2ETests/V1/Controllers/WorkflowsControllerTests.cs
@@ -124,14 +124,16 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
         output.WriteLine($"Workflow {workflow.Name} is {workflow.Status} after " + sw.ElapsedMilliseconds + "ms");
         Assert.True(isRunning);
 
-        // Get the logstream using rest-assured-net
-        Given()
+        // Get the logstream using rest-assured-net. Make sure it isn't waiting for the workflow to finish
+        HttpResponseMessage ranResponse = Given()
             .UseHttpCompletionOption(HttpCompletionOption.ResponseHeadersRead)
             .Accept("application/x-ndjson")
             .When()
             .Get($"{RootUrl}/api/v1/workflows/{workflow.Name}/pods/{workflow.Name}/logstream")
             .Then()
-            .StatusCode(HttpStatusCode.OK)
+            .Extract().Response();
+        output.WriteLine(JsonConvert.SerializeObject(ranResponse));
+            // .StatusCode(HttpStatusCode.OK)
             // .And()
             // .Header("content-type", "application/x-ndjson")
             // .And()
@@ -139,8 +141,8 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
             // Istio strips this header. Not sure why yet. See https://defense-unicorns.slack.com/archives/C06QJAUHWFN/p1722893232750909
             // .And()
             // .Header("Connection", "keep-alive")
-            .And()
-            .ResponseTime(NHamcrest.Is.LessThan(TimeSpan.FromSeconds(10)));
+            // .And()
+            // .ResponseTime(NHamcrest.Is.LessThan(TimeSpan.FromSeconds(10)));
 
         // Get the logstream. Make sure it isn't waiting for the workflow to finish
         // We have to do our own request here because rest-assured-net doesn't support SSE
