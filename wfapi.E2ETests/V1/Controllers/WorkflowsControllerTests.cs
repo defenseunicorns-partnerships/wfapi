dfce7d4cd4138cce3d37159cbe54e7b1ba98c826
Luke Griswold <ldgriswold@defenseunicorns.com>
2024-10-30 15:29:56 -0400

    chore: longer time for running logs check

--- wfapi.E2ETests/V1/Controllers/WorkflowsControllerTests.cs
+++ wfapi.E2ETests/V1/Controllers/WorkflowsControllerTests.cs
@@ -288,7 +288,7 @@ public class WorkflowsControllerTests(ITestOutputHelper output)
             // .And()
             // .Header("Connection", "keep-alive")
             .And()
-            .ResponseTime(Is.LessThan(TimeSpan.FromSeconds(12)));
+            .ResponseTime(Is.LessThan(TimeSpan.FromSeconds(24)));
 
         // Do it one more time. This time it should happen extremely quickly since we know the pod is done initializing.
         HttpResponseMessage response = Given()
